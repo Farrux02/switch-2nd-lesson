8b8ba7c22c725d1bddc948529aa8cdddbccb48d4
Farruh <farruh020202@gmail.com>
2022-08-29 15:54:58 +0500

    done

---
+++ script.js
@@ -0,0 +1,39 @@
+// let number = +prompt("Введите номер для экстренного вызова");
+
+// switch (number) {
+//   case 101:
+//     alert("Номер пожарной безопасности");
+//     break;
+//   case 102:
+//     alert("Номер для вызова милиции");
+//     break;
+//   case 103:
+//     alert("Номер скорой помощи");
+//     break;
+//   case 104:
+//     alert("Номер аварийной службы газа");
+//     break;
+//   case 1050:
+//     alert("Номер мчс");
+//     break;
+//   default:
+//     alert("Вы ввели неверное значение");
+//     break;
+// }
+
+// задача для while. Найти сумму всех четных чисел
+
+// var max = +prompt('Введите число');
+// let min = 0;
+
+// var sum = 0;
+
+// while (min <= max) {
+//   if( min % 2 === 0) {
+//     let num = min + min;
+//     sum = num + min;
+//   }
+//   min++;
+// }
+
+// console.log(sum);
